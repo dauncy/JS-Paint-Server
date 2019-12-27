a3d4b5defa95694b800f5416d70bdfb293af156f
Daniel Wilder <dauncy@gmail.com>
2019-12-27 13:01:35 -0800

    database

--- JS_Paint/db/schema.rb
+++ JS_Paint/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "challenges", force: :cascade do |t|
     t.string "name"
     t.string "description"
